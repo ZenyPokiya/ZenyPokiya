f8b4162054d154b04c2793ec16e5521189890d2b
hardik <kantariyahardik618@ggamil.com>
2024-06-27 11:52:41 +0530

    add

--- cpp/co6.cpp
+++ cpp/co6.cpp
@@ -5,7 +5,40 @@ class report{
     private:
     int adno;
     char name[20];
-    };
+    int marks[5];
+    float average;
+    float getavg(int sub);
     
-
-int main(){}
+    
+    public:
+    void readinfo(){
+        cout<<"ad no:";
+        cin>>adno;
+        cout<<"Name:";
+        cin>>name;
+        cout<<"Marks:";
+        cin>>marks;
+    }
+    void getavg(){
+        cout<<"adno:"<<adno;
+        cout<<"Name:"<<name;
+        cout<<"Marks"<<marks;
+        cout<<"Average:"<<average;
+        cout<<"Average of five sub:"<<getavg(sub);
+    }
+    void displayinfo(){
+         cout<<"adno:"<<adno;
+        cout<<"Name:"<<name;
+        cout<<"Marks"<<marks;
+        cout<<"Average:"<<average;
+        cout<<"Average of five sub:"<<getavg( int sub);
+    }
+};
+float report::getavg(int sub){
+    return sub/5;
+}
+int main(){
+    class report rt;
+    rt.readinfo();
+    rt.displayinfo();
+}
