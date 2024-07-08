1eb76c43ea4dc2d3a631d8f05d26ee718ac16a02
ZenyPokiya <sq.zeny26@gmail.com>
2024-07-08 20:42:25 +0530

    sdaf

--- project/restaurant.cpp
+++ project/restaurant.cpp
@@ -3,30 +3,30 @@ using namespace std;
 
 class Menu{
     public:
-    int price,qty;
+    int price,qty,dno;
     public:
+    void data(){
+    cout<<"********** MENU ***********"<<endl;
+    cout<<"        ITEM                              PRICE "<<endl;
+    cout<<"(1)  Chicken Biryani                    $2 only."<<endl;
+    cout<<"(2)  Chicken Palao                      $1.5 only."<<endl;
+    cout<<"(3)  Chinese Rice                       $2.5 only."<<endl;
+    cout<<"(4)  Chicken Burgar                     $1 only."<<endl;
+    cout<<"(5)  Nawabi Pizza                       $4.5 only."<<endl;
+    cout<<"(6)  2.5 Litre Coke                     $1.75 only."<<endl;
+        }
+
     void order(){
+    
     cout<<"Enter dish number:";
+    cin>>dno;
     cout<<"Enter qty:";
+    cin>>qty;
     }
 
     void Bill(){
-        cout<<"Price:"<<price*qty;
+        cout<<"Price:"<<price*qty<<endl;
     }
-
-    void data(){
-    cout<<"********** MENU ***********";
-    cout<<"        ITEM                              PRICE ";
-    cout<<"(1)  Chicken Biryani                    $2 only.";
-    cout<<"(2)  Chicken Palao                      $1.5 only.";
-    cout<<"(3)  Chinese Rice                       $2.5 only.";
-    cout<<"(4)  Chicken Burgar                     $1 only.";
-    cout<<"(5)  Nawabi Pizza                       $4.5 only.";
-    cout<<"(6)  2.5 Litre Coke                     $1.75 only.";
-        
-    }
-    
-    
 };
 
 int main()
@@ -37,7 +37,7 @@ int main()
     mn.data();
     int n;
     do{
-    cout<<"****************************";
+    cout<<"****************************"<<endl;
     cout<<endl<<endl<<"1.Take Order"<<endl;
     cout<<endl<<"2.Generate Bill"<<endl;
     cout<<endl<<"3.Show Menu"<<endl;
@@ -45,9 +45,9 @@ int main()
     cout<<"Enter selected number:";
     cin>>n;
 
-    switch(n){
+     switch(n){
        case 1:
-       
+       cout<<"";
        cout;
        break;
        case 2:
@@ -60,11 +60,16 @@ int main()
        cout<<endl<<"********* THANK YOU **********";
        cout<<endl<<"********* VISIT AGAIN ***********";
        break;
-
-    }
-
-
-   
+       default:
+       ;
+       
+     }
+    }while(n!=4);
+    for(int i=1;i<=0;i++){
+        if(i==5){
+           continue; 
+        }
+        cout<<i;
     }
 return 0;
 }
