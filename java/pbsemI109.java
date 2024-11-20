626293d2ed3171e07329083e272348c13b55594e
ZenyPokiya <sq.zeny26@gmail.com>
2024-11-20 22:44:29 +0530

    jdgf

---
+++ java/pbsemI109.java
@@ -0,0 +1,14 @@
+class pbsemI109
+{
+    public static void main(String args[])
+    {
+        int b=10,c=20;
+        float a;
+        boolean d;
+        d=b<c;
+        b+=++b;
+        c-=--c;
+        a=(d)?b:c;
+        System.out.print(a+'a');
+    }
+}
